3067185fcca1daa59fdb189676f362061fc1bda4
Fer3D <ferbg9@gmail.com>
2025-11-26 00:23:59 +0100

    Animaciones Ataque en diferentes direcciones

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,6 +14,11 @@ public class Player : MonoBehaviour
 
     private bool gameIsPaused = false;
 
+    private bool isAttacking = false;
+    private bool canMove = true;
+
+    Vector2 lasMovementDir = Vector2.right;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -25,6 +30,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (isAttacking)
+            canMove = false;
+        else
+            canMove = true;
+
+        if (movementInput != Vector2.zero)
+        {
+            lasMovementDir = movementInput;
+        }
+
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
 
@@ -38,11 +53,20 @@ public class Player : MonoBehaviour
         OpenCloseInventory();
 
         OpenClosePauseMenu();
+
+        Attack();
     }
 
     private void FixedUpdate()
     {
-        rb2D.linearVelocity = movementInput * speed;
+        if (canMove)
+        {
+            rb2D.linearVelocity = movementInput * speed;
+        }
+        else
+        {
+            rb2D.linearVelocity = Vector2.zero;
+        }
     }
 
     void CheckFlip()
@@ -77,4 +101,63 @@ public class Player : MonoBehaviour
             }
         }
     }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            int dir = GetDirectionIndex(lasMovementDir);
+            Vector2 attackDir = GetAttackInputDirection();
+            int attackDirection = GetDirectionIndex(attackDir);
+
+            animator.SetInteger("AttackDirection", attackDirection);
+
+            int randomIndex = Random.Range(0, 2);
+            animator.SetInteger("AttackIndex", randomIndex);
+            animator.SetTrigger("DoAttack");
+        }
+    }
+
+    public void StartAttack()
+    {
+        isAttacking = true;
+    }
+
+    public void EndAttack()
+    {
+        isAttacking = false;
+    }
+    Vector2 GetAttackInputDirection()
+    {
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            return inputDir;
+        }
+        else
+        {
+            if (transform.localScale.x > 0)
+            {
+                return Vector2.right;
+            }
+            else
+            {
+                return Vector2.left;
+            }
+        }
+    }
+
+    int GetDirectionIndex(Vector2 dir)
+    {
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            return dir.x > 0 ? 0 : 1;
+        }
+        else
+        {
+            return dir.y > 0 ? 2 : 3;
+        }
+    }
+    
 }
