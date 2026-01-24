71480c52cb39ada0569f49441b36c9407fe82a7b
Fer3D <ferbg9@gmail.com>
2026-01-24 21:50:03 +0100

    Screen shake al recibir un golpe. FEELS

--- Assets/Scripts/Player/DamageReceiverPlayer.cs
+++ Assets/Scripts/Player/DamageReceiverPlayer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
 
 public class DamageReceiverPlayer : MonoBehaviour
 {
@@ -11,6 +12,15 @@ public class DamageReceiverPlayer : MonoBehaviour
     private Animator animator;
 
     public float forceImpulse = 5;
+    [Header("Cinemachine")]
+    [Range(0f, 3f)]
+    [Tooltip("Multiplicate the shake force.")]
+    public float shakeForce = 1f;
+    [Tooltip("Additional multiplier to scale the Cinemachine impulse signal.")]
+    public float shakeMultiplier = 1f;
+    [SerializeField]
+    [Tooltip("Assign a Cinemachine Impulse Source if you want manual control; it will be added automatically if missing.")]
+    private CinemachineImpulseSource impulseSource;
 
     public AudioSource audioDie;
 
@@ -18,6 +28,15 @@ public class DamageReceiverPlayer : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+        if (impulseSource == null)
+        {
+            impulseSource = GetComponentInChildren<CinemachineImpulseSource>();
+        }
+        if (impulseSource == null)
+        {
+            impulseSource = gameObject.AddComponent<CinemachineImpulseSource>();
+        }
 
         currentHealth = maxHealth;
         UIManager.instance.UpdateHealth(currentHealth, maxHealth);
@@ -28,6 +47,11 @@ public class DamageReceiverPlayer : MonoBehaviour
     {
         currentHealth -= amount;
         UIManager.instance.UpdateHealth(currentHealth, maxHealth);
+
+        if (impulseSource != null)
+        {
+            impulseSource.GenerateImpulse(hitDirection.normalized * shakeForce * shakeMultiplier);
+        }
 
         if (applyForceOrNot)
         {
